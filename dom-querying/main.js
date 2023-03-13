b67a1013d5ea6c55d250ae4061ad33262177b32a
Louis Zhang <Louisgeemod@gmail.com>
2023-03-13 18:14:20 +0000

    queryed dom for specific target

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('Hello,World');
+var h1 = document.querySelector('h1');
+console.log('$heading', h1);
+console.dir(h1);
+
+var explanation = document.querySelector('#explanation');
+console.log('explanation is ', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('hint is ', hint);
+console.dir(hint);
+
+var list = document.querySelectorAll('p');
+console.log('list is ', list);
+
+var example = document.querySelectorAll('.example-link');
+console.log('example is ', example);
